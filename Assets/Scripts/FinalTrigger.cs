25b21e845e7db88982e1932085010f44fe533099
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-07-15 22:00:50 +0800

    Add Working Game end
    "Fully functional" level!

--- Assets/Scripts/FinalTrigger.cs
+++ Assets/Scripts/FinalTrigger.cs
@@ -1,18 +1,43 @@
+/******************************************************************************
+Author: Leong Yu Xuan
+
+Name of Class: FinalTrigger
+
+Description of Class: This class is for the trigger at the end of the level.
+                        Freeze Player movement and show end screen
+                        
+
+Date Created: 14/07/2021
+******************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinalTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    ///<summary>
+    ///Variable to store player obj to freeze
+    /// </summary>
+    public GameObject Player;
 
-    // Update is called once per frame
-    void Update()
+    ///<summary>
+    ///Variable to store UI Canvas that stores Demo Over screen
+    /// </summary>
+    public GameObject FinalScreen;
+
+    ///<summary>
+    ///Variable to store main player UI Canvas 
+    /// </summary>
+    public GameObject PlayerUI;
+
+    ///<summary>
+    ///Function for doing what is stated in the Heading
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
     {
-        
+        FinalScreen.SetActive(true);
+        PlayerUI.SetActive(false);
+        Player.GetComponent<SamplePlayer>().CanMove = false;
     }
 }
